7d20c1e2c8674bfeb120b6da4b593a36261a8f38
Azirude <taylorocks123@gmail.com>
2014-09-26 12:56:19 -0600

    Added quitchecker.

--- src/chatbot/controller/ChatboxAppController.java
+++ src/chatbot/controller/ChatboxAppController.java
@@ -2,27 +2,35 @@ package chatbot.controller;
 
 import javax.swing.JOptionPane;
 
+import chatbot.model.Chatbot;
 import chatbot.view.ChatBotView;
 
 public class ChatboxAppController
 {
 	private ChatBotView applicationView;
-	
+	private Chatbot mySillyChatbot;
+
 	public ChatboxAppController()
 	{
 		applicationView = new ChatBotView(this);
+		mySillyChatbot = new Chatbot("derf");
 	}
-	
+
 	public void start()
 	{
+
+		/**
+		 * Calls the Chatbot textbox, and initializes name as Taylor.
+		 */
+
 		String result = applicationView.showChatbot(" Taylor ");
-		
-		if(result.equalsIgnoreCase("exit"))
+
+		if (mySillyChatbot.quitChecker(result))
 		{
 			quit();
 		}
 	}
-	
+
 	private void quit()
 	{
 		JOptionPane.showMessageDialog(null, "He's dead, jim!");
--- src/chatbot/model/Chatbot.java
+++ src/chatbot/model/Chatbot.java
@@ -2,39 +2,65 @@ package chatbot.model;
 
 public class Chatbot
 {
-	
+
 	private String name;
 	private int chatCount;
 	private String length;
-	
+
+	/**
+	 * creates a Chatbot object with the supplied name and initializes the
+	 * current number of chats to zero.
+	 * 
+	 * @param name
+	 *            The supplied name for the Chatbot.
+	 */
+
 	public Chatbot(String name)
 	{
 		this.name = name;
 		chatCount = 0;
 	}
-	
+
+	/**
+	 * Returns the name of the Chatbot object.
+	 * 
+	 * @return the current name of the Chatbot.
+	 */
+
 	public String getName()
 	{
 		return name;
 	}
-	
+
 	public String getLength()
 	{
 		return length;
 	}
-	
+
 	public int getChatCount()
 	{
 		return chatCount;
 	}
-	
+
 	public void setName(String name)
 	{
 		this.name = name;
 	}
-	
+
 	private void updateChatCount()
 	{
 		chatCount++;
+	}
+
+	public boolean quitChecker(String input)
+	{
+		boolean okToQuit = false;
+
+		if (input.equals("Goodbye"))
+		{
+			okToQuit = true;
+		}
+
+		return okToQuit;
 	}
 }
